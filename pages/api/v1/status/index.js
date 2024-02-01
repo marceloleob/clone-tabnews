34e60c876dcb00794163b3d0b2bfda7753ae0807
Marcelo Leopold <marceloleob@gmail.com>
2024-02-01 13:58:27 -0500

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-  response.status(200).json({ chave: "alunos do curso.dev são legais" });
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log(result);
+  response.status(200).json({ chave: "Teste de porta" });
 }
 
 export default status;
