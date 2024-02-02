8122446463071ef81975dbbd27b74c35e483a31e
Marcelo Leopold <marceloleob@gmail.com>
2024-02-02 13:46:41 -0500

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,35 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result);
-  response.status(200).json({ chave: "Teste de porta" });
+  const updatedAt = new Date().toISOString();
+
+  // 1) versao do Postgres
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
+  // 2) conexoes maximas
+  const dbMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const dbMaxConnectionsValue = dbMaxConnectionsResult.rows[0].max_connections;
+  // 3) conexoes usadas
+  const dbName = process.env.POSTGRES_DB;
+  const dbOpenConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+  const dbOpenConnectionsValue = dbOpenConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        status: "",
+        max_connections: parseInt(dbMaxConnectionsValue),
+        opened_connections: dbOpenConnectionsValue,
+        latency: {},
+        version: dbVersionValue,
+      },
+      webserver: {},
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,17 @@
 test("GET to api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3002/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+
+  const db = responseBody.dependencies.database;
+  // versao do Postgres
+  expect(db.version).toEqual("16.0");
+  // conexoes maximas
+  expect(db.max_connections).toEqual(100);
+  // conexoes usadas
+  expect(db.opened_connections).toEqual(1);
 });
